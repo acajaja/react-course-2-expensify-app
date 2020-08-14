6d6c15dd817e937a15f9c6a591a9a56e3dbe05e0
CJ3 <acajaja@jazzopetry.net>
2020-08-13 20:45:08 -0400

    Set up for Heroku deployment. Removed Edit link from header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ const Header = (props) => (
         <nav>
             <NavLink to="/" activeClassName="is-active" title="Home" exact={true}>Home</NavLink>
             <NavLink to="/create" activeClassName="is-active" title="Create">Create</NavLink>
-            <NavLink to="/edit" activeClassName="is-active" title="Edit">Edit</NavLink>
             <NavLink to="/help" activeClassName="is-active" title="Help">Help</NavLink>
         </nav>
     </header>
