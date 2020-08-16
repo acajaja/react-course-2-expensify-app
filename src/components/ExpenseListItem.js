0463fdf7862b4cbad668b3fd77426dee62bc5728
CJ3 <acajaja@jazzopetry.net>
2020-08-15 23:33:34 -0400

    Added date & currency formatting. Also updated Header test snapshot.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <section>
             <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
-            <p>${amount} - created at: {createdAt}</p>
+            <p>
+                {numeral(amount / 100).format('$0,0.00')} -
+                created at: {moment(createdAt).format('MMMM Do, YYYY')}
+            </p>
         </section>
     </div>
 );
 
 export default ExpenseListItem;
-
