a4324d9a572f1797ae77df1c94bae37c7eb134d1
CJ3 <acajaja@jazzopetry.net>
2020-08-22 10:30:49 -0400

    Fixed typo

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const webpack = require('webpack');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
-    require('gotenv').config({ path: '.env.test' });
+    require('dotenv').config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
-    require('gotenv').config({ path: '.env.development' });
+    require('dotenv').config({ path: '.env.development' });
 }
 
 module.exports = (env, argv) => {
